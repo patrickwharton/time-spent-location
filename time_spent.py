5042d80470ef44819297b3227053ae99a1017728
Patrick <patrickwharton1@gmail.com>
2019-03-01 03:16:46 +0000

    fixed zip func

--- time_spent.py
+++ time_spent.py
@@ -5,7 +5,6 @@ from zipfile import ZipFile
 
 # TODO
 """
-Running with zip file input
 Add in lat/lon functionality
 ask for home location (+lat/lon)
 add in total life time spent
@@ -29,16 +28,57 @@ def time_spent(rootdir = "/home/patrick/Documents/PSFeb19", HOMEINCLUDED = False
     home = "GB"
 
     if rootdir[-4:] == ".zip":
-        location_list = zip(rootdir)
+        trips = zip(rootdir, HOMEINCLUDED, homelat, homelon, home)
+
     else:
-        location_list = dir(rootdir)
+        trips = dir(rootdir, HOMEINCLUDED, homelat, homelon, home)
+
+    if not trips:
+        return None
+
+    return helper(trips, HOMEINCLUDED)
+
+
+def zip(file_name, HOMEINCLUDED, homelat, homelon, home):
+    with ZipFile(file_name, 'r') as zip:
+        # printing all the contents of the zip file
+        location_list = []
+        for f in zip.namelist():
+            if f[-9:] == "trip.json":
+                location_list.append(f)
+
+        if not location_list:
+            return None
+
+        trips = []
+        j = 0
+        for file in location_list:
+            trips.append([])
+            data = json.loads(zip.read(file))
+            for i in data["all_steps"]:
+                trips[j].append([i["start_time"], i["location"]["country_code"],
+                            i["location"]["lon"], i["location"]["lat"]])
+            if HOMEINCLUDED:
+                end = data["end_date"]
+                trips[j].append([end, home, homelon, homelat])
+            j += 1
+    return trips
+
+
+def dir(rootdir, HOMEINCLUDED, homelat, homelon, home):
+    rootdir = rootdir + "/trip/**/*"
+    file_list = [f for f in glob.iglob(rootdir, recursive=True) if os.path.isfile(f)]
+    location_list = []
+    for f in file_list:
+        if f[-9:] == "trip.json":
+            location_list.append(f)
 
     if not location_list:
         return None
 
     trips = []
     j = 0
-    accounted_for = []
+
     for file in location_list:
         trips.append([])
         with open(file, "r") as f:
@@ -50,7 +90,11 @@ def time_spent(rootdir = "/home/patrick/Documents/PSFeb19", HOMEINCLUDED = False
                 end = data["end_date"]
                 trips[j].append([end, home, homelon, homelat])
         j += 1
+    return trips
 
+
+def helper(trips, HOMEINCLUDED):
+    accounted_for = []
     countries = {}
     for trip in trips:
         start_time = None
@@ -73,28 +117,7 @@ def time_spent(rootdir = "/home/patrick/Documents/PSFeb19", HOMEINCLUDED = False
             if not started:
                 started = start_time
         accounted_for.append([started, start_time])
-    return countries
-
-def zip(file_name):
-    with ZipFile(file_name, 'r') as zip:
-        # printing all the contents of the zip file
-        location_list = []
-        for f in zip.namelist():
-            if f[-9:] == "trip.json":
-                location_list.append(f)
-        print(location_list)
-    return location_list
-
-def dir(rootdir):
-    rootdir = rootdir + "/trip/**/*"
-    file_list = [f for f in glob.iglob(rootdir, recursive=True) if os.path.isfile(f)]
-    location_list = []
-    for f in file_list:
-        if f[-9:] == "trip.json":
-            location_list.append(f)
-    return location_list
-
-
+    return countries, accounted_for
 
 if __name__=="__main__":
     time_spent()
